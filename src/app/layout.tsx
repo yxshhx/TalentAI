736cbae605fc4b4340ada032b771de2754bf5dc1
YASH SOLANKI <2203031050819@paruluniversity.ac.in>
2025-08-28 10:21:22 +0000

    CAN YOU MAKE IT MORE FUNCTIONAL LIKE UI AND ALL THE FEATURES I NEED Pro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
 export const metadata: Metadata = {
   title: 'TalentAI',
@@ -16,15 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className={cn('font-body antialiased')}>
+      <body className={cn('antialiased font-sans', inter.variable)}>
         {children}
         <Toaster />
       </body>
